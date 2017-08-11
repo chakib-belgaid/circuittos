337cc99997fbe35a02326e4893294aa417b73e53
chakib <bm_belgaid@esi.dz>
2017-08-11 18:03:00 +0100

    woking with services

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StoresComponent } from './stores/stores.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StoresComponent
   ],
   imports: [
     BrowserModule
